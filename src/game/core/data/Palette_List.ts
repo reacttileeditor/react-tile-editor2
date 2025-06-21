74634e3c917cd839fb68a91f27b5ca0adf7a5963
Richard Kettering <kettering.richard@gmail.com>
2025-06-20 19:59:54 -0500

    Prepping a set of initial palettes, and associated markup.

--- src/game/core/data/Palette_List.ts
+++ src/game/core/data/Palette_List.ts
@@ -1,4 +1,5 @@
-
+import convert from "color-convert";
+import { HEX } from "color-convert";
 
 export type Palette_Names = 'team1' | 'team2' | 'team3';
 
@@ -8,18 +9,71 @@ export type Palette_List = {
 };
 
 export type Palette_Data = {
-	val: number,
+	pairs: Array<Color_Pair>,
 };
 
 export const palette_list: Palette_List = {
 	'team1': {
-		val: 20,
+		pairs: [
+			['400133', '400a01'],
+			['50013f', '500d01'],
+			['5f014b', '5f0f01'],
+			['6c0155', '6c1101'],
+			['86016a', '861401'],
+			['980178', '981701'],
+			['b2088e', 'b22108'],
+			['c618a3', 'c62f18'],
+			['dd30b9', 'dd4830'],
+			['eb47c9', 'eb5e47'],
+			['ff55db', 'ff6e55'],
+			['fb80e0', 'fb9380'],
+			['ffa1eb', 'ffafa1'],
+			['ffbff1', 'ffc9bf'],
+			['ffd2f5', 'ffd9d2'],
+			['ffe9fa', 'ffede9']
+		],
 	},
 	'team2': {
-		val: 100,
+		pairs: [
+			['400133', '400a01'],
+			['50013f', '500d01'],
+			['5f014b', '5f0f01'],
+			['6c0155', '6c1101'],
+			['86016a', '861401'],
+			['980178', '981701'],
+			['b2088e', 'b22108'],
+			['c618a3', 'c62f18'],
+			['dd30b9', 'dd4830'],
+			['eb47c9', 'eb5e47'],
+			['ff55db', 'ff6e55'],
+			['fb80e0', 'fb9380'],
+			['ffa1eb', 'ffafa1'],
+			['ffbff1', 'ffc9bf'],
+			['ffd2f5', 'ffd9d2'],
+			['ffe9fa', 'ffede9']
+		],
 	},
 	'team3': {
-		val: 200
+		pairs: [
+			['400133', '400a01'],
+			['50013f', '500d01'],
+			['5f014b', '5f0f01'],
+			['6c0155', '6c1101'],
+			['86016a', '861401'],
+			['980178', '981701'],
+			['b2088e', 'b22108'],
+			['c618a3', 'c62f18'],
+			['dd30b9', 'dd4830'],
+			['eb47c9', 'eb5e47'],
+			['ff55db', 'ff6e55'],
+			['fb80e0', 'fb9380'],
+			['ffa1eb', 'ffafa1'],
+			['ffbff1', 'ffc9bf'],
+			['ffd2f5', 'ffd9d2'],
+			['ffe9fa', 'ffede9']
+		],
 	},
 
 }
+
+type Color_Pair = [HEX, HEX];
