7d854524b8d79dc618ec3b8e710c91dfd25e568b
Richard Kettering <kettering.richard@gmail.com>
2025-07-03 21:34:56 -0500

    Tweaked the movement tooltip a little.

--- src/game/core/gui/Game_Components/Game_Tooltip_Manager.tsx
+++ src/game/core/gui/Game_Components/Game_Tooltip_Manager.tsx
@@ -55,7 +55,11 @@ const Map_Tooltip = (props: Game_Tooltip_Data) => {
 		if(props.selected_unit !== undefined){
 			if(props.hovered_unit ){
 				if( equals(props.tile_pos, props.unit_pos) ){
-					return 'Move (Cancel)'
+					if( size(props.path_data?.path_this_turn) ){
+						return 'Cancel Move'
+					} else {
+						return 'n/a'
+					}
 				} else {
 					//tile_pos implicitly is the currently selected unit, so this is a different one.
 					return 'Select Unit'
